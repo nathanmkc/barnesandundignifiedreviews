ca3b3b31a08a7999f5b163eea8ad9be34703dbaa
nathanmkc <nkellehe8@gmail.com>
2021-01-13 16:17:01 -0800

    removed react from bundle

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoUri = 'mongodb://username:password@18.220.174.46:27017/barnesandundignified';
+const mongoUri = 'mongodb://nathan:sillysyl8@18.220.174.46:27017/barnesandundignified';
 
 const db = mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
